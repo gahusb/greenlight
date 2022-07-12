e2eb2333d80bd877801864bc7c88b8169c45ca6b
이대직 <30641972+leedjik@users.noreply.github.com>
2022-07-12 22:19:17 +0900

    gradle로 변경

---
+++ BackEnd/greenlight/src/main/java/com/greenlight/GreenlightApplication.java
@@ -0,0 +1,13 @@
+package com.greenlight;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GreenlightApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GreenlightApplication.class, args);
+    }
+
+}
--- BackEnd/greenlight/src/test/java/com/greenlight/GreenlightApplicationTests.java
+++ BackEnd/greenlight/src/test/java/com/greenlight/GreenlightApplicationTests.java
@@ -1,13 +1,13 @@
-package com.example.backend;
+package com.greenlight;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class BackEndApplicationTests {
+class GreenlightApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
