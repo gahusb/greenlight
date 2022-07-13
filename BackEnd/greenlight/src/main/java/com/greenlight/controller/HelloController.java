4eda75cfcb8b86821e373a5f003d431d2eaf79b2
이대직 <30641972+leedjik@users.noreply.github.com>
2022-07-14 00:07:51 +0900

    Member Service 구현

--- BackEnd/greenlight/src/main/java/com/greenlight/controller/HelloController.java
+++ BackEnd/greenlight/src/main/java/com/greenlight/controller/HelloController.java
@@ -1,8 +1,6 @@
 package com.greenlight.controller;
 
-import lombok.Getter;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -10,11 +8,11 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @Controller
 public class HelloController {
 
-    @GetMapping("hello")
-    @ResponseBody
-    public Hello hello(@RequestParam("name") String name){
-        Hello hello = new Hello();
-        hello.setName(name);
-        return hello;
-    }
+//    @GetMapping("hello")
+//    @ResponseBody
+//    public Hello hello(@RequestParam("name") String name){
+//        Hello hello = new Hello();
+//        hello.setName(name);
+//        return hello;
+//    }
 }
