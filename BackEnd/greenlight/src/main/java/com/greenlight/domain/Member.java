b3c81a3bb063161b41a2b9a6cd67feab466cc3df
이대직 <30641972+leedjik@users.noreply.github.com>
2022-07-17 01:21:38 +0900

    Member Service 기능 구현 중

--- BackEnd/greenlight/src/main/java/com/greenlight/domain/Member.java
+++ BackEnd/greenlight/src/main/java/com/greenlight/domain/Member.java
@@ -1,30 +1,29 @@
 package com.greenlight.domain;
 
-import org.springframework.stereotype.Controller;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
-import javax.persistence.*;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
+@Getter
+@NoArgsConstructor
 @Entity
 public class Member {
-    @Id
+    @Id//PK
     @GeneratedValue(strategy = GenerationType.IDENTITY)// DB가 알아서 autoincrement
     private Long id;
 
     private String name;
 
-    public Long getId() {
-        return id;
-    }
+    private boolean donated;
 
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
+    @Builder
+    public Member(String name, boolean donated){
         this.name = name;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
+        this.donated = donated;
     }
 }
